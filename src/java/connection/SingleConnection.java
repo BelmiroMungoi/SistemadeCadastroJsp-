a1aade4d45777465920befeb6c84fc8a8c3b02c4
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2020-12-29 23:26:21 +0200

    Metodo responsavel pela conexao com a base de dados

---
+++ src/java/connection/SingleConnection.java
@@ -0,0 +1,46 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Belmiro-Mungoi
+ */
+public class SingleConnection {
+
+    private static String url = "jdbc:mysql://localhost:3306/projectoJsp?autoReconnect=true";
+    private static String user = "root";
+    private static String password = "";
+    private static String driver = "com.mysql.jdbc.Driver";
+    private static Connection connection = null;
+
+    static {
+        try {
+            connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public SingleConnection() {
+        connect();
+    }
+
+    private static void connect() {
+        try {
+            if (connection == null) {
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, password);
+                connection.setAutoCommit(false);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao conectar a base de dados");
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
