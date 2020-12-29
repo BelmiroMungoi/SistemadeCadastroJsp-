62ec56474054e90cd079c1fad3877a6d888ffae5
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2020-12-29 23:23:04 +0200

    Getters e setters do usuario

---
+++ src/java/beans/BeansUsuario.java
@@ -0,0 +1,64 @@
+package beans;
+
+/**
+ *
+ * @author Belmiro-Mungoi
+ */
+public class BeansUsuario {
+
+    private String nomeCompleto;
+    private String biUser;
+    private String telefone;
+    private String nomeUser;
+    private String senha;
+    private Integer idUser;
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getBiUser() {
+        return biUser;
+    }
+
+    public void setBiUser(String biUser) {
+        this.biUser = biUser;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNomeUser() {
+        return nomeUser;
+    }
+
+    public void setNomeUser(String nomeUser) {
+        this.nomeUser = nomeUser;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Integer getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Integer idUser) {
+        this.idUser = idUser;
+    }
+
+}
