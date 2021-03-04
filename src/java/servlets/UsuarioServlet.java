21084fe102c5bed0757a55a6e0af7797cc57b313
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-05 00:12:13 +0200

    adicionando uma miniatura de imagema para usuario

--- src/java/servlets/UsuarioServlet.java
+++ src/java/servlets/UsuarioServlet.java
@@ -2,10 +2,12 @@ package servlets;
 
 import beans.BeansUsuario;
 import dao.DaoUsuario;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import javax.imageio.ImageIO;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -25,28 +27,28 @@ import org.apache.tomcat.util.codec.binary.Base64;
 @WebServlet("/UsuarioServlet")
 @MultipartConfig
 public class UsuarioServlet extends HttpServlet {
-    
+
     DaoUsuario daoUsuario = new DaoUsuario();
-    
+
     public UsuarioServlet() {
         super();
     }
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         try {
             String accao = request.getParameter("accao");
             String id = request.getParameter("user");
-            
+
             if (accao.equals("delete")) {
                 daoUsuario.excluir(id);
                 RequestDispatcher view
                         = request.getRequestDispatcher("/cadastroUsuario.jsp");
                 request.setAttribute("usuarios", daoUsuario.listarUsuario());
                 view.forward(request, response);
-                
+
             } else if (accao.equals("edit")) {
                 BeansUsuario beansUsuario = daoUsuario.consulta(id);
                 RequestDispatcher view
@@ -54,31 +56,31 @@ public class UsuarioServlet extends HttpServlet {
                 request.setAttribute("user", beansUsuario);
                 request.setAttribute("usuarios", daoUsuario.listarUsuario());
                 view.forward(request, response);
-                
+
             } else if (accao.equals("listar")) {
                 RequestDispatcher view
                         = request.getRequestDispatcher("/cadastroUsuario.jsp");
                 request.setAttribute("usuarios", daoUsuario.listarUsuario());
                 view.forward(request, response);
-                
+
             } else if (accao.equals("download")) {
                 BeansUsuario beansUsuario = daoUsuario.consulta(id);
-                
+
                 if (beansUsuario != null) {
                     String type = request.getParameter("type");
                     String contentType = "";
                     byte[] files = null;
-                    
+
                     if (type.equals("imagem")) {
                         contentType = beansUsuario.getContentType();
                         // Converte a img na base64 para um byte[] 
                         files = new Base64().decodeBase64(beansUsuario.getImagem());
-                        
+
                     } else if (type.equals("curriculo")) {
                         contentType = beansUsuario.getContentTypeCv();
                         files = new Base64().decodeBase64(beansUsuario.getCurriculo());
                     }
-                    
+
                     response.setHeader("Content-Disposition", "attachment;filename="
                             + beansUsuario.getNomeCompleto() + "."
                             + contentType.split("\\/")[1]);
@@ -89,7 +91,7 @@ public class UsuarioServlet extends HttpServlet {
                     int read = 0;
                     byte[] bs = new byte[1024];
                     OutputStream output = response.getOutputStream();
-                    
+
                     while ((read = input.read(bs)) != -1) {
                         output.write(bs, 0, read);
                     }
@@ -101,11 +103,11 @@ public class UsuarioServlet extends HttpServlet {
             e.printStackTrace();
         }
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         boolean podeInserir = true;
         String accao = request.getParameter("accao");
         //Cancela e reseta a pag
@@ -118,7 +120,7 @@ public class UsuarioServlet extends HttpServlet {
                 e.printStackTrace();
             }
         } else {
-            
+
             String idUsuario = request.getParameter("idUser");
             String nomeComp = request.getParameter("nomeFull");
             String biUsuario = request.getParameter("biUser");
@@ -126,29 +128,40 @@ public class UsuarioServlet extends HttpServlet {
             String nomeUsuario = request.getParameter("nomeUser");
             String senha = request.getParameter("pass");
             BeansUsuario usuario = new BeansUsuario();
-            
+
             usuario.setIdUser(!idUsuario.isEmpty() ? Integer.parseInt(idUsuario) : null);
             usuario.setNomeCompleto(nomeComp);
             usuario.setBiUser(biUsuario);
             usuario.setTelefone(fone);
             usuario.setNomeUser(nomeUsuario);
             usuario.setSenha(senha);
-            
+
             try {
                 //Inicio doCodigo para carregar ficheiros(fotos/pdfs) da base de dados
                 if (ServletFileUpload.isMultipartContent(request)) { //Verifica se e um form de upload
 
                     Part imagem = request.getPart("foto");
+
                     if (imagem != null && imagem.getInputStream().available() > 0) {
-                        String fotoBase64 = new Base64().encodeBase64String(
-                                converteStream(imagem.getInputStream()));
+
+                        byte[] imagemByte = converteStream(imagem.getInputStream());
+                        String fotoBase64 = new Base64().encodeBase64String(imagemByte);
                         usuario.setImagem(fotoBase64);
                         usuario.setContentType(imagem.getContentType());
+
+                        //Codigo para a compactacao da imagem para sua exibicao na tela
+                        BufferedImage bufferedImage = ImageIO.read(
+                                new ByteArrayInputStream(imagemByte));
+                        int tipo = bufferedImage.getType() == 0
+                                ? BufferedImage.TYPE_INT_ARGB : bufferedImage.getType();
+                        BufferedImage newImage = new BufferedImage(32, 32, tipo);
+                        //Fim do codigo de compactar a imagem
                     } else {
+
                         usuario.setImagem(request.getParameter("fotoUser"));
                         usuario.setContentType(request.getParameter("content"));
                     }
-                    
+
                     Part curriculo = request.getPart("curriculo");
                     if (curriculo != null && curriculo.getInputStream().available() > 0) {
                         String curriculoBase64 = new Base64().encodeBase64String(
@@ -164,53 +177,53 @@ public class UsuarioServlet extends HttpServlet {
                 if (nomeComp == null || nomeComp.isEmpty()) {
                     request.setAttribute("msg", "Preencha o Campo Nome Completo!");
                     podeInserir = false;
-                    
+
                 } else if (biUsuario == null || biUsuario.isEmpty()) {
                     request.setAttribute("msg", "Preencha o Campo BI!");
                     podeInserir = false;
-                    
+
                 } else if (fone == null || fone.isEmpty()) {
                     request.setAttribute("msg", "Preencha o Campo Telefone!");
                     podeInserir = false;
-                    
+
                 } else if (nomeUsuario == null || nomeUsuario.isEmpty()) {
                     request.setAttribute("msg", "Preencha o Campo Nome do Usuário!");
                     podeInserir = false;
-                    
+
                 } else if (senha == null || senha.isEmpty()) {
                     request.setAttribute("msg", "Preencha o Campo Senha!");
                     podeInserir = false;
-                    
+
                 } else if (idUsuario == null || idUsuario.isEmpty()
                         && !daoUsuario.validaUsuario(nomeUsuario)) {
                     request.setAttribute("msg", "Nome de Usuario ja existe, TENTE OUTRO!!!");
                     podeInserir = false;
-                    
+
                 } else if (idUsuario == null || idUsuario.isEmpty()
                         && !daoUsuario.validaSenha(senha)) {
                     request.setAttribute("msg", "Essa senha ja existe, TENTE OUTRA!!!");
                     podeInserir = false;
-                    
+
                 } else if (idUsuario == null || idUsuario.isEmpty()
                         && daoUsuario.validaUsuario(nomeUsuario) && podeInserir) {
                     daoUsuario.salvarUsuario(usuario);
-                    
+
                 } else if (idUsuario != null && !idUsuario.isEmpty()) {
-                    
+
                     if (!daoUsuario.validaUsuarioUpdate(nomeUsuario, idUsuario)) {
                         request.setAttribute("msg", "Nome de Usuario ja existe, TENTE OUTRO!!!");
                         podeInserir = false;
-                        
+
                     } else if (!daoUsuario.validaSenhaUpdate(senha, idUsuario)) {
                         request.setAttribute("msg", "Essa Senha Já Existe, TENTE OUTRA!!!");
                         podeInserir = false;
-                        
+
                     } else if (podeInserir) {
                         daoUsuario.actualiza(usuario);
                     }
-                    
+
                 }
-                
+
                 if (!podeInserir) {
                     request.setAttribute("user", usuario);
                 }
@@ -220,7 +233,7 @@ public class UsuarioServlet extends HttpServlet {
                 // exibir dados na tela
                 request.setAttribute("usuarios", daoUsuario.listarUsuario());
                 view.forward(request, response);
-                
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -237,5 +250,5 @@ public class UsuarioServlet extends HttpServlet {
         }
         return outputStream.toByteArray();
     }
-    
+
 }
