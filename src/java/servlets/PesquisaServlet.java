d31d438425ee8612b51d51bc9f9de16e205e9f2e
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-14 11:36:53 +0200

    Adicionando o codigo para fazer a pesquisa e setar na tela

--- src/java/servlets/PesquisaServlet.java
+++ src/java/servlets/PesquisaServlet.java
@@ -1,6 +1,10 @@
 package servlets;
 
+import beans.BeansUsuario;
+import dao.DaoUsuario;
 import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +18,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/PesquisaServlet")
 public class PesquisaServlet extends HttpServlet {
 
+    private DaoUsuario daoUsuario = new DaoUsuario();
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -21,6 +27,10 @@ public class PesquisaServlet extends HttpServlet {
         String pesquisa = request.getParameter("pesquisa");
 
         if (pesquisa != null) {
+            List<BeansUsuario> lista = daoUsuario.pesquisaUsuario(pesquisa);
+            RequestDispatcher view = request.getRequestDispatcher("/cadastroUsuario.jsp");
+            request.setAttribute("usuarios",lista);
+            view.forward(request, response);
 
         }
     }
