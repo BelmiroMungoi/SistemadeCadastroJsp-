b600c29b3b1d5be8356f6194fee848faf008033f
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-14 11:42:14 +0200

    Getters e Setters para categoria

---
+++ src/java/beans/BeansCategoria.java
@@ -0,0 +1,28 @@
+package beans;
+
+/**
+ *
+ * @author Belmiro-Mungoi
+ */
+public class BeansCategoria {
+
+    private Integer idCat;
+    private String nomeCat;
+
+    public Integer getIdCat() {
+        return idCat;
+    }
+
+    public void setIdCat(Integer idCat) {
+        this.idCat = idCat;
+    }
+
+    public String getNomeCat() {
+        return nomeCat;
+    }
+
+    public void setNomeCat(String nomeCat) {
+        this.nomeCat = nomeCat;
+    }
+
+}
